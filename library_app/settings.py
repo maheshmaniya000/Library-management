272d69daea7049bfde7915db027a857951f0fff5
Mahesh Maniya <maheshmaniya000@gmail.com>
2025-12-19 20:49:19 +0530

    trusted origin

--- library_app/settings.py
+++ library_app/settings.py
@@ -28,7 +28,9 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://library-management-dk5w.onrender.com",
+]
 # Application definition
 
 INSTALLED_APPS = [
